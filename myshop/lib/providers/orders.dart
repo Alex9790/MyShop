79344813625bbe327465a29e6c2a4f604567b100
Darkestt <martinez.alex.9790@gmail.com>
2020-05-21 22:02:15 -0400

    10.21 se agrega funcionalidad para almacenar en Firebase los pedidos realizados

--- myshop/lib/providers/orders.dart
+++ myshop/lib/providers/orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+//libreria que provee herramientas para convertir data / permite usar json.encode()
+import 'dart:convert';
 
 import '../providers/cart.dart';
 
@@ -26,10 +29,37 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder (List<CartItem> cartProducts, double total){
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    //URL base de firebase mas la collection donde se almacenan los productos
+    const url = "https://flutter-update-d1853.firebaseio.com/Orders.json";
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        "amount": total,
+        //uniform string representation of dates, facilita convertir de vuelta a Datetime cuando recibes el dato
+        "dateTime": timestamp.toIso8601String(),
+        //para poder guardar en Firebase se debe convertir la lista de CartItem en una lista de Maps
+        "products": cartProducts.map((cartProduct) => {
+          "id": cartProduct.id,
+          "title": cartProduct.title,
+          "quantity": cartProduct.quantity,
+          "price": cartProduct.price
+        }).toList(),
+      }),
+    );
+
     //metodo de agregar elemento en la posicion(index) definido y los demas elementos se corren una posicion
     //en este caso todos los nuevos pedidos se insertan al principio de la lista
-    _orders.insert(0, OrderItem(id: DateTime.now().toString(), amount: total, products: cartProducts, dateTime: DateTime.now()));
+    _orders.insert(
+        0,
+        OrderItem(
+          //para obtener el id generado en firebase
+          id: json.decode(response.body)["name"], 
+          amount: total,
+          products: cartProducts,
+          dateTime: timestamp,
+        ));
     notifyListeners();
   }
 }
--- myshop/lib/screens/cart_screen.dart
+++ myshop/lib/screens/cart_screen.dart
@@ -46,25 +46,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(
-                    onPressed: () {
-                      //Se accede a la lista de Pedidos y se le agrega un nuevo pedido
-                      //en vez de pasar todo el Map se pasa solo la lista de productos
-                      //no queremos escuchar aqui porque solo queremos registrar un pedido
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      //se eliminan los items del carrito
-                      cart.clear();
-                    },
-                    child: Text(
-                      "Order Now!",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  ),
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -86,6 +68,56 @@ class CartScreen extends StatelessWidget {
                 cart.items.values.toList()[index].title),
           ))
         ],
+      ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      //se desactiva el boton si no hay elementos en el carrito o si esta cargando un pedido
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading) 
+      ? null : () async {
+        setState(() {
+          _isLoading = true;
+        });
+        //Se accede a la lista de Pedidos y se le agrega un nuevo pedido
+        //en vez de pasar todo el Map se pasa solo la lista de productos
+        //no queremos escuchar aqui porque solo queremos registrar un pedido
+        await Provider.of<Orders>(context, listen: false).addOrder(
+          widget.cart.items.values.toList(),
+          widget.cart.totalAmount,
+        );
+
+        setState(() {
+          _isLoading = false;
+        });
+        
+        //se eliminan los items del carrito
+        widget.cart.clear();
+      },
+      //se muestra spinner de loading en vez de texto cuando se estan guardando pedidos
+      child: _isLoading ? CircularProgressIndicator() : Text(
+        "Order Now!",
+        style: TextStyle(
+          color: Theme.of(context).primaryColor,
+        ),
       ),
     );
   }
