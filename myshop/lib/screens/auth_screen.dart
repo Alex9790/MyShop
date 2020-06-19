54e48c07a77e0701fbf0375c10dce18b2d90757e
Darkestt <martinez.alex.9790@gmail.com>
2020-06-19 17:16:15 -0400

    12.4 se cambia AnimatedBuilder() por AnimatedContainer() el cual es util para animar containers sin necesidad de declarar controladores ni animaciones por fuera, todo se define dentro del Widget

--- myshop/lib/screens/auth_screen.dart
+++ myshop/lib/screens/auth_screen.dart
@@ -222,21 +222,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, child) => Container(
-          //height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
+      child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeIn,
+          height: _authMode == AuthMode.Signup ? 320 : 260,
+          //height: _heightAnimation.value.height,
           constraints:
-              //BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-              BoxConstraints(minHeight: _heightAnimation.value.height),
+              BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+              //BoxConstraints(minHeight: _heightAnimation.value.height),
           width: deviceSize.width * 0.75,
           padding: EdgeInsets.all(16.0),
-          child: child,
-        ),
-        //Form() no se actualizara cuando se reconstruya el Widget, asi que se coloca en el argumento "child" de AnimatedBuilder() 
-        //y se recibe en el argumento "builder" en el parametro "child"
-        child: Form(
+          child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
