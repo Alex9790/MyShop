ddd28a2a5cde742fdf70e868ec8aef25b89c67da
Darkestt <martinez.alex.9790@gmail.com>
2020-06-22 14:57:14 -0400

    12.9 Se agrega animacion a la lista de pedidos, cuando se expande para ver los detalles del mismo

--- myshop/lib/widgets/order_item.dart
+++ myshop/lib/widgets/order_item.dart
@@ -22,59 +22,67 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text("\$${widget.order.amount}"),
-            subtitle: Text(
-              DateFormat("dd/MM/yyyy hh:mm").format(widget.order.dateTime),
-            ),
-            //Boton para expandir el pedido y ver mas detalles del mismo
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            //si entiendo bien, se ajusta a la cantidad de productos que tiene (si son pocos, e mostraran sin espacio extra vacio) y un maximo de 180
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 180),
-              child: ListView(
-                //se deben mostrar los datos de cada "CartItem" en un Widget, para ello se usa .map() que optiene cada "CartItem" y retorna Widgets
-                children: widget.order.products
-                    .map(
-                      (producto) => Row(
-                        //para incluir espacio entre los Widgets contenidos
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            producto.title,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "${producto.quantity}x \$${producto.price}",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                    .toList(),
+    return AnimatedContainer(
+      duration: Duration(
+        milliseconds: 300,
+      ),
+      height: _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text("\$${widget.order.amount}"),
+              subtitle: Text(
+                DateFormat("dd/MM/yyyy hh:mm").format(widget.order.dateTime),
+              ),
+              //Boton para expandir el pedido y ver mas detalles del mismo
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
             ),
-        ],
+            //if (_expanded)
+              //si entiendo bien, se ajusta a la cantidad de productos que tiene (si son pocos, e mostraran sin espacio extra vacio) y un maximo de 180
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300,),
+              //Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                height: _expanded ? min(widget.order.products.length * 20.0 + 10, 180) : 0,
+                child: ListView(
+                  //se deben mostrar los datos de cada "CartItem" en un Widget, para ello se usa .map() que optiene cada "CartItem" y retorna Widgets
+                  children: widget.order.products
+                      .map(
+                        (producto) => Row(
+                          //para incluir espacio entre los Widgets contenidos
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              producto.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "${producto.quantity}x \$${producto.price}",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
